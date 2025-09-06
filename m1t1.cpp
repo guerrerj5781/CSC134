a500702eec14fa3756217794345c739ca606c92a
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-09-06 19:23:17 +0000

    Finshed m1t1

---
+++ m1t1.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M1T1
+guerreroj
+9/6/25 
+Program will greet the user.
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, user!" << endl;
+
+    return 0; // no errors
+}
