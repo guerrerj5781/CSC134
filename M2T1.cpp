7a6163356400bdfc3d50522fc14a70aa1a022205
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-09-20 16:12:39 -0400

    Update M2T1.cpp

--- M2T1.cpp
+++ M2T1.cpp
@@ -17,25 +17,7 @@ int main() {
     double cost_each = 0.99; 
     double total_cost; 
 
-
-    cout << "Welcome to our " << product << " store!" << endl;
-    cout << "What's your first name? ";
-    cin >> first_name; 
-    cout << "What's your last name? ";
-    cin >> last_name;
-    full_name = first_name + " " + last_name; 
-    cout << "Nice to meet you, " << full_name << endl;
-
-    cout << "How many " << product << " would you like today? ";
-    cin >> amount_purchased;
-
-    total_cost = amount_purchased * cost_each;
-
-    cout << setprecision(2) << fixed; 
-
-    cout << "For " << amount_purchased << " " << product << endl;
-    cout << "That will be: $" << total_cost << endl;
-    cout << "Thank you for shopping with us!" << endl;
+    
 
     return 0;
 }
