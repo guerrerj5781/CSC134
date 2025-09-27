564aebac66ac1a322376ccde4db48aae71796145
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-09-27 23:41:05 +0000

    M2T2 Final

--- M2T2.cpp
+++ M2T2.cpp
@@ -6,7 +6,7 @@ September 27th, 2025
 */
 
 #include <iostream>
-#include <iomanip>  // for 2 decimal places trick
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -17,13 +17,19 @@ int main() {
     double tax_amount;
     double total;
 
-    cout << "Welcome to our Potato Restaurant!" << endl;
+    cout << "Welcome to our Spud Farm!" << endl;
     cout << "You ordered one " << item << "." << endl;
 
     tax_amount = item_price * tax_percent;
     total = item_price + tax_amount;
 
     cout << setprecision(2) << fixed;
+    cout << "Thank you for shopping with us" << endl;
+    cout << "------------------------------" << endl;
+    cout << item << "\t$" << item_price      << endl;
+    cout << "Tax" << "\t\t$" << tax_amount   << endl;
+    cout << "------------------------------" << endl; 
+    cout << "Total" << "\t\t$" << total << endl;
     cout << total << endl;
 
     return 0;
