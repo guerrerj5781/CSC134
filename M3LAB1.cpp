2d91d86d6dac8080a10f2c91e88c3111b413b551
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-10-04 20:27:44 +0000

    M3LAB1 Start

---
+++ M3LAB1.cpp
@@ -0,0 +1,12 @@
+// CSC 134
+// M3LAB1 - Example 3: The Office Edition
+// Guerrero, Jeremy
+// 10/3/2025
+
+#include <iostream>
+using namespace std;
+
+void chooseOption1();
+void chooseOption2();
+void chooseOption3();
+
