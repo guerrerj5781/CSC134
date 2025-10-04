2af8f557cc3b0af52f96893f82f439e7a1d4ccb2
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-10-04 19:10:58 +0000

    M2LAB Complete

--- M2LAB.cpp
+++ M2LAB.cpp
@@ -11,11 +11,40 @@ using namespace std;
 
 int main() {
 
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
 
 
+    double length,
+            width,
+            height,
+            volume,
+            cost,
+            charge,
+            profit;
 
 
+        cout << setprecision(2) << fixed << showpoint;
 
-    
+        cout << "Enter the dimensions of the crate (in feet):\n";
+        cout << "Length: " << endl; 
+        cin >> length;
+        cout << "Width: " << endl;
+        cin >> width;
+        cout << "Height: " << endl;
+        cin >> height;
+
+        volume = length * width * height;
+        cost = volume * COST_PER_CUBIC_FOOT;
+        charge = volume * CHARGE_PER_CUBIC_FOOT;
+        profit = charge - cost;
+
+        cout << "The volume of the crate is: ";
+        cout << volume << " cubic feet.\n";
+        cout << "Cost to build: $" << cost << endl;
+        cout << "Charge to customer: $" << charge << endl;
+        cout << "Profit: $" << profit << endl;
+
+
     return 0;
 }
