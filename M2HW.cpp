ffb5f44be6f85d3910bfea411a6edc134a7ea168
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-10-04 18:28:07 +0000

    M2HW Test 1

--- M2HW.cpp
+++ M2HW.cpp
@@ -4,3 +4,32 @@ M2HW1 - GOLD
 GuerreroJ
 October 4th, 2025
 */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+using namespace std;
+
+int main() {
+    cout << fixed << setprecision(2);  // format money to 2 decimals
+
+    cout << "\n===== Question 1: Banking Transactions =====\n";
+    string name;
+    double startBal{}, deposit{}, withdrawal{};
+    cout << "Enter account holder name: ";
+    getline(cin, name);
+    cout << "Enter starting balance: $";
+    cin >> startBal;
+    cout << "Enter deposit amount: $";
+    cin >> deposit;
+    cout << "Enter withdrawal amount: $";
+    cin >> withdrawal;
+
+
+
+
+    return 0;
+}
