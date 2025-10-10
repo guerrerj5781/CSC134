4ae6d4c0d0e6d545a76952a382fe91c790800af9
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-10-10 20:07:52 +0000

    M3LAB2 Moving Along

--- M3LAB2.cpp
+++ M3LAB2.cpp
@@ -10,6 +10,20 @@ using namespace std;
 
 int main() {
 
+    // Declare variables
+    int grade;          // stores the users input grade
+    char letterGrade;   // stores the calculated letter grade
 
-    
+    // Ask user for input
+    cout << "Enter your numerical grade: ";
+    cin >> grade;
+
+    // Validate input using boolean logic
+    // Checks if grade is outside 0–100 range
+    if (grade < 0 || grade > 100) {
+        cout << "Invalid grade. Please enter a number between 0 and 100." << endl;
+        return 1; // exits program early
+
+
+    return 0;  
 }
