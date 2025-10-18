121686e289549de215fe79e2d1b3f499bb7d30a0
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-10-18 15:32:47 +0000

    Getting Started

--- M4T1.cpp
+++ M4T1.cpp
@@ -10,8 +10,19 @@ using namespace std;
 
 int main() {
 
+    // Program 5-3
+    // Using a simple while loop
 
+    int number = 5;   // total number of times to print "Hello"
+    int i = 0;        // loop counter starts at zero
 
+    while (i < number) {
+        cout << "Hello" << endl;     // output message
+
+        i++;
+    }
+
+    cout << "That's all!" << endl;   // printed once after loop ends
 
     return 0;
 }
