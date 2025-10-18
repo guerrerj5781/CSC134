c1202a6beba762e2d692cbfcdc35bbd3395ea702
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-10-18 14:33:45 +0000

    M3HW getting started

---
+++ M3HW.cpp
@@ -0,0 +1,21 @@
+/*
+CSC 134
+M3HW1 - Gold
+GuerreroJ
+10/18/25
+*/
+
+#include <iostream>   // For cin and cout
+#include <iomanip>    // For setw and setprecision
+#include <string>     // For string type
+#include <cstdlib>    // For rand() and srand()
+#include <ctime>      // For time()
+using namespace std;
+
+int main()
+{
+
+
+    
+    return 0;
+}
