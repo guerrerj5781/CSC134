44df17d62e2e70b82513f003fd8121ebeff94d75
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-11-01 15:17:44 +0000

    M4HW Complete

--- M4HW.cpp
+++ M4HW.cpp
@@ -5,6 +5,9 @@ GuerreroJ
 11/1/25
 */
 
+// Program prints a multiplication table for any number between 1 & 12.
+
+
 #include <iostream>
 using namespace std;
 
@@ -16,7 +19,16 @@ int main() {
     cout << "Enter a number from 1 to 12: ";
     cin >> number;
 
+    while (number < 1 || number > 12) {
+        cout << "Invalid input. Please enter a number between 1 and 12: ";
+        cin >> number;
+    }
     
+    // Print the times table
+    while (i <= 12) {
+        cout << number << " times " << i << " is " << number * i << "." << endl;
+        i++;
+    }
 
     return 0;
 }
