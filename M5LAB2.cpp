23671df18b1471bcb2159cd804d405389900bde5
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-11-10 19:18:59 +0000

    halfway

--- M5LAB2.cpp
+++ M5LAB2.cpp
@@ -5,3 +5,24 @@ GuerreroJ
 11/10/2025
 */
 
+#include <iostream>
+using namespace std;
+
+// Function prototypes
+double getLength();
+double getWidth();
+double getArea(double length, double width);
+void displayData(double length, double width, double area);
+
+int main()
+{
+    // Variable declarations
+    double length,   // The rectangle's length
+           width,    // The rectangle's width
+           area;     // The rectangle's area
+
+    // Get the rectangle's length
+    length = getLength();
+
+    // Get the rectangle's width
+    width = getWidth();
