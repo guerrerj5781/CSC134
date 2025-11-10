0a6b8b3e9f5cea9398a5d0fc3ad8b77d9ba38649
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-11-10 18:00:22 +0000

    Data Created

--- M5LAB_A_GuerreroJ.cpp
+++ M5LAB_A_GuerreroJ.cpp
@@ -6,11 +6,11 @@ GuerreroJ
      Inlcude's all three exercises
 */
 
-    // ===== My Game Theme (notes) =====
-    // Theme: Survival Horror – Space (Alien, 1979, USCSS Nostromo, Weyland-Yutani)
-    // Health: Vitals (body status) + Psyche (stress tolerance)
-    // Ranks: Warrant Officer Grades (1–10)  (using level 1–10 for labs)
-    // Items: Flamethrower, Motion Tracker, Cattle Prod, EVA Suit, Emergency Beacon
+// ===== My Game Theme (notes) =====
+// Theme: Survival Horror – Space (Alien, 1979, USCSS Nostromo, Weyland-Yutani)
+// Health: Vitals (body status) + Psyche (stress tolerance)
+// Ranks: Warrant Officer Grades (1–10)  (using level 1–10 for labs)
+// Items: Flamethrower, Motion Tracker, Cattle Prod, EVA Suit, Emergency Beacon
 
 
 
@@ -18,3 +18,18 @@ GuerreroJ
 #include <string>
 #include <limits>
 using namespace std;
+
+// ----------------------------- Data -----------------------------
+string crewName = "Ellen Ripley";   // Sigourney Weaver, Alien (1979)
+int    rankLvl  = 5;                // “level”
+int    vitals   = 80,  maxVitals = 100;
+int    psyche   = 60,  maxPsyche = 100;
+
+const int NUM_ITEMS = 5;
+string equipment[NUM_ITEMS] = {
+    "Incinerator Unit",
+    "Motion Tracker",
+    "Cattle Prod",
+    "EVA Suit",
+    "Harpoon Grapple Gun"
+};
