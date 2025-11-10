11d1cdbe24cf294bfd47cefe5ad82b91c52b12f4
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-11-10 19:24:01 +0000

    Tested and adjusted

--- M5LAB2.cpp
+++ M5LAB2.cpp
@@ -8,7 +8,7 @@ GuerreroJ
 #include <iostream>
 using namespace std;
 
-// Function prototypes
+// function prototypes
 double getLength();
 double getWidth();
 double getArea(double length, double width);
@@ -16,13 +16,55 @@ void displayData(double length, double width, double area);
 
 int main()
 {
-    // Variable declarations
-    double length,   // The rectangle's length
-           width,    // The rectangle's width
-           area;     // The rectangle's area
+    // variable declarations
+    double length,   // rectangle's length
+           width,    // rectangle's width
+           area;     // rectangle's area
 
-    // Get the rectangle's length
+    // get the rectangle's length
     length = getLength();
 
-    // Get the rectangle's width
+    // get the rectangle's width
     width = getWidth();
+
+    // calculate the area
+    area = getArea(length, width);
+
+    // display the rectangle's data
+    displayData(length, width, area);
+
+    return 0;
+}
+
+// getLength - ask user to enter the rectangle's length and returns it
+double getLength()
+{
+    double length;
+    cout << "Enter the rectangle's length: ";
+    cin >> length;
+    return length;
+}
+
+// getWidth - ask user to enter the rectangle's width and returns it
+double getWidth()
+{
+    double width;
+    cout << "Enter the rectangle's width: ";
+    cin >> width;
+    return width;
+}
+
+// getArea - calculates and returns the rectangle's area
+double getArea(double length, double width)
+{
+    return length * width;
+}
+
+// displayData - displays the rectangle's data
+void displayData(double length, double width, double area)
+{
+    cout << "\n--- Rectangle Data ---\n";
+    cout << "Length: " << length << endl;
+    cout << "Width : " << width << endl;
+    cout << "Area  : " << area << endl;
+}
