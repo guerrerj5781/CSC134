1874515aecaecac2c0f13b803e3778d5514d0221
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-11-10 17:57:42 +0000

    M5LAB_A Started

---
+++ M5LAB_A_GuerreroJ.cpp
@@ -0,0 +1,20 @@
+/*
+CSC134
+M5LabA
+GuerreroJ
+11/10/2025
+     Inlcude's all three exercises
+*/
+
+    // ===== My Game Theme (notes) =====
+    // Theme: Survival Horror – Space (Alien, 1979, USCSS Nostromo, Weyland-Yutani)
+    // Health: Vitals (body status) + Psyche (stress tolerance)
+    // Ranks: Warrant Officer Grades (1–10)  (using level 1–10 for labs)
+    // Items: Flamethrower, Motion Tracker, Cattle Prod, EVA Suit, Emergency Beacon
+
+
+
+#include <iostream>
+#include <string>
+#include <limits>
+using namespace std;
