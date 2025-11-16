d247961537f3f19b43e8704359895241abe5202e
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-11-16 18:28:43 +0000

    Question 3 Complete

--- M5HW.cpp
+++ M5HW.cpp
@@ -120,3 +120,39 @@ void blockVolume() {
     cout << fixed << setprecision(2);
     cout << "The volume of the block is " << volume << " cubic units.\n";
 }
+
+// -----------------------------------------
+// Question 3 – Roman Numeral Converter
+// -----------------------------------------
+void romanNumeral() {
+    int number;
+
+    cout << "\n=== Question 3: Roman Numeral Converter ===\n";
+
+    cout << "Enter a number (1 - 10): ";
+    cin >> number;
+
+    // Input Validation: 1–10
+    if (number < 1 || number > 10) {
+        cout << "Error: number must be between 1 and 10.\n";
+        return;
+    }
+
+    string roman;
+
+    switch (number) {
+        case 1:  roman = "I";    break;
+        case 2:  roman = "II";   break;
+        case 3:  roman = "III";  break;
+        case 4:  roman = "IV";   break;
+        case 5:  roman = "V";    break;
+        case 6:  roman = "VI";   break;
+        case 7:  roman = "VII";  break;
+        case 8:  roman = "VIII"; break;
+        case 9:  roman = "IX";   break;
+        case 10: roman = "X";    break;
+    }
+
+    cout << "The Roman numeral version of " << number
+         << " is " << roman << ".\n";
+}
