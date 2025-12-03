c0523fde1f304752d37beffbc6bf8f0f4ef45379
guerrerj5781 <guerrerj5781@student.faytechcc.edu>
2025-12-03 22:37:10 +0000

    Getting started

--- M6T1.cpp
+++ M6T1.cpp
@@ -5,3 +5,28 @@ GuerreroJ
 12/03/2025
 */
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// Function declarations
+void part1();
+void part2();
+
+int main() {
+    cout << fixed << setprecision(2);
+
+    cout << "=== M6T1: Car Counts at Construction Site ===\n\n";
+
+    cout << "--- Part 1: Loop only (no arrays) ---\n";
+    part1();
+
+    cout << "\n------------------------------------------\n\n";
+
+    cout << "--- Part 2: Loop with arrays + bar chart ---\n";
+    part2();
+
+    cout << "\nDone.\n";
+    return 0;
+}
